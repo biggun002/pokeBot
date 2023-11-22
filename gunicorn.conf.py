270c6c057606eab29ff6898f4f889793c1074fac
biggun002 <biggun002@gmail.com>
2023-11-22 15:37:21 +0700

    add gunicorn.conf.py to read env

---
+++ gunicorn.conf.py
@@ -0,0 +1,8 @@
+# gunicorn.conf.py
+import os
+from dotenv import load_dotenv
+
+for env_file in ('.env', '.flaskenv'):
+    env = os.path.join(os.getcwd(), env_file)
+    if os.path.exists(env):
+        load_dotenv(env)
